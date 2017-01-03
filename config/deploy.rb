30c5ff32f88628e71624bda22b84c8f808892875
theoandtheb <blue.apple@sympatico.ca>
2017-01-03 15:24:23 -0500

    Revised the Capistrano deployment config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,12 +14,13 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to, 	  '/opt/www/cbsc'
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to true if using ActiveRecord
@@ -34,6 +35,7 @@ set :puma_init_active_record, true  # Change to true if using ActiveRecord
 
 # Default value for :pty is false
 set :pty, true
+set :use_sudo,        false
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml config/secrets.yml}
