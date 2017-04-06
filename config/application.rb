01214b289272fe6690abefd7202e407c8bb06e82
theoandtheb <blue.apple@sympatico.ca>
2017-04-06 17:55:02 -0400

    Updated the application config to enable CORS from the client site.

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module Cbsc
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+	    'Access-Control-Allow-Origin' => 'http://www.cbsc.ca/',
+	    'Access-Control-Request-Method' => 'POST'
+  	}
   end
 end
